7a0808be0ec8ce29eb7f2d7c47b9147c04fbe350
Ivan Humeniuk <rt072@tutanota.com>
2019-05-11 20:24:33 +0000

    change script for openvpn status

--- polybar/scripts/vpn-openvpn-isrunning.sh
+++ polybar/scripts/vpn-openvpn-isrunning.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-if [ "$(pgrep openvpn)" ]; then
-    echo "VPN %{F#859900}%{F-}"
+if pgrep -x "openvpn" > /dev/null
+then
+echo "VPN %{F#859900}%{F-}"
 else
-    echo ""
+echo "VPN %{F#DC322F}%{F-}"
 fi
+
+# if pgrep -x "gedit" > /dev/null
