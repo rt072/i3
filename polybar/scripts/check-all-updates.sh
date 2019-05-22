3eb5518b8995c68fb4fea8ee3297666d694a0b10
Ivan Humeniuk <rt072@tutanota.com>
2019-05-22 19:58:36 +0000

    change script

--- polybar/scripts/check-all-updates.sh
+++ polybar/scripts/check-all-updates.sh
@@ -1,23 +1,35 @@
 #!/bin/sh
 #source https://github.com/x70b1/polybar-scripts
 
+if ping -c 4 -q 1.1.1.1 >/dev/null 2>&1
+then
+    if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
+        updates_arch=0
+    fi
 
-if ! updates_arch=$(checkupdates 2> /dev/null | wc -l ); then
-    updates_arch=0
-fi
+    if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
+        updates_aur=0
+    fi
 
-# if ! updates_aur=$(cower -u 2> /dev/null | wc -l); then
-if ! updates_aur=$(trizen -Su --aur --quiet | wc -l); then
-    updates_aur=0
-fi
+    updates=$(("$updates_arch" + "$updates_aur"))
+    echo $updates > /tmp/updates.txt
+    file='/tmp/updates.txt'
+    upd=`head -n 1 /tmp/updates.txt`
 
-updates=$(("$updates_arch" + "$updates_aur"))
-echo $updates > /tmp/updates.txt
-file='/tmp/updates.txt'
-upd=`head -n 1 /tmp/updates.txt`
-
-if [ -s "$file" ]; then
-    echo " $upd"
+    if [ -s "$file" ]; then
+        echo " $upd"
+    else
+        echo " 0"
+    fi
 else
-    echo " 0"
+    updates=$(("$updates_arch" + "$updates_aur"))
+    echo $updates > /tmp/updates.txt
+    file='/tmp/updates.txt'
+    upd=`head -n 1 /tmp/updates.txt`
+
+    if [ -s "$file" ]; then
+        echo " $upd"
+    else
+        echo " 0"
+    fi
 fi
